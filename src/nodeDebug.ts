43de8d02d9bdcb5674f95ad0e49f1427de22549c
Rob Lourens <roblourens@gmail.com>
2016-08-26 13:55:57 -0700

    Update to latest -core package

--- src/nodeDebug.ts
+++ src/nodeDebug.ts
@@ -2,6 +2,13 @@
  * Copyright (C) Microsoft Corporation. All rights reserved.
  *--------------------------------------------------------*/
 
-import {ChromeDebugSession} from 'vscode-chrome-debug-core';
+import {ChromeDebugSession, logger} from 'vscode-chrome-debug-core';
+import * as path from 'path';
 
-ChromeDebugSession.run(ChromeDebugSession);
+ChromeDebugSession.run(ChromeDebugSession.getSession(
+    {
+        logFilePath: path.resolve(__dirname, '../../vscode-node-cdp-debug.txt') // non-.txt file types can't be uploaded to github
+    }));
+
+/* tslint:disable:no-var-requires */
+logger.log('debugger-for-chrome: ' + require('../../package.json').version);
