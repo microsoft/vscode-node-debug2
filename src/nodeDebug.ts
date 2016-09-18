e40d61866cd854551487e5284b99b46dc475cd5f
Rob Lourens <roblourens@gmail.com>
2016-09-18 17:27:07 +0200

    Pass class, not instance of adapter impl

--- src/nodeDebug.ts
+++ src/nodeDebug.ts
@@ -2,7 +2,7 @@
  * Copyright (C) Microsoft Corporation. All rights reserved.
  *--------------------------------------------------------*/
 
-import {ChromeDebugSession, chromeTargetDiscoveryStrategy, logger} from 'vscode-chrome-debug-core';
+import {ChromeDebugSession, logger} from 'vscode-chrome-debug-core';
 import * as path from 'path';
 
 import {NodeDebugAdapter} from './nodeDebugAdapter';
@@ -10,7 +10,7 @@ import {NodeDebugAdapter} from './nodeDebugAdapter';
 ChromeDebugSession.run(ChromeDebugSession.getSession(
     {
         logFilePath: path.resolve(__dirname, '../../vscode-node-cdp-debug.txt'), // non-.txt file types can't be uploaded to github
-        adapter: new NodeDebugAdapter(),
+        adapter: NodeDebugAdapter,
         extensionName: 'node-debug'
     }));
 
