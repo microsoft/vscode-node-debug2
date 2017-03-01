16670a980bb70d85430e053b77150887d277568e
Rob Lourens <roblourens@gmail.com>
2017-03-01 15:21:33 -0800

    Undo app.ts deletes

--- testapp/app.ts
+++ testapp/app.ts
@@ -1,12 +1,10 @@
 import * as app2 from './app2';
 
-
-async function log() {
+function log() {
     console.log('Hello, world!');
     test();
 }
 log();
-
 setInterval(log, 3000);
 
 function test() {
@@ -27,6 +25,81 @@ function test() {
         app2.consoleAPIs();
         app2.throwCaught();
     }
+}
+
+async function foo(): Promise<void> {
+    await Promise.resolve('foo');
+    await Promise.resolve('bar');
+}
+
+async function locals() {
+    await foo();
+    const veryLargeArray = [];
+    veryLargeArray['blah'] = 5;
+    for (let i = 0; i <= 1000; i++) veryLargeArray[i] = Math.random();
+    veryLargeArray[0] = veryLargeArray;
+
+    let generator = function*() {
+        for (let i=0; i<10; i++)
+            yield i;
+    };
+    const gen = generator();
+    const genNext = gen.next();
+    const promise = Promise.resolve("sdfkj");
+
+    const manyPropsObj: any = { prop2: 'abc', prop1: 'def' };
+    for (let i=0; i<=5; i++) manyPropsObj['prop' + i] = Math.random();
+
+    const reg = /^asdf.*$/g;
+    const longStr = `this is a
+string with
+newlines`;
+    const buffer = new ArrayBuffer(8);
+    const buffView = new Int32Array(buffer);
+    buffView[0] = 234;
+    const s = Symbol('hi');
+    const e = new Error('hi');
+
+    const handler = {
+        get: function(target, name){
+            return name in target?
+                target[name] :
+                37;
+        }
+    };
+
+    const num = 5.1;
+    const p = { proxy: new Proxy({b: 4}, handler) };
+
+    const p2 = new Proxy({a: 1} ,handler);
+
+    const m = new Map();
+    m.set('a', 1);
+    m.set('bcd', [1, 2, 3]);
+    m.set('def', m);
+    m['blah'] = 3;
+    m[5] = 1;
+
+    const set = new Set();
+    set.add('blah');
+    set.add([1, 2, 3]);
+    set.add(set);
+
+    let nan = NaN;
+    let inf = 1/0;
+    let infStr = "Infinity";
+    const obj = {
+        '[test]': 1,
+        '.test2': 2,
+        ' ': 3,
+        5: 1
+    };
+
+    const bool = true;
+    const fn = () => {
+        // Some fn
+        const xyz = 321;
+        app2.anotherFn();
         return 5;
     };
 
