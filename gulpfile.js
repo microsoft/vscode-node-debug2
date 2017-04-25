963e8ad94c9e8915622344058e86f3de12df6b09
Rob Lourens <roblourens@gmail.com>
2017-04-25 10:03:31 -0700

    Add transifex tasks

--- gulpfile.js
+++ gulpfile.js
@@ -17,6 +17,23 @@ const fs = require('fs');
 const vsce = require('vsce');
 const es = require('event-stream');
 
+const transifexApiHostname = 'www.transifex.com'
+const transifexApiName = 'api';
+const transifexApiToken = process.env.TRANSIFEX_API_TOKEN;
+const transifexProjectName = 'vscode-extensions';
+const transifexExtensionName = 'vscode-node-debug';
+const vscodeLanguages = [
+    'zh-hans',
+    'zh-hant',
+    'ja',
+    'ko',
+    'de',
+    'fr',
+    'es',
+    'ru',
+    'it'
+];
+
 const watchedSources = [
 	'src/**/*',
     'test/**/*'
@@ -117,4 +134,21 @@ gulp.task('publish', function(callback) {
 
 gulp.task('package', function(callback) {
 	runSequence('build', 'add-i18n', 'vsce-package', callback);
+});
+
+gulp.task('transifex-push', function () {
+    return gulp.src('**/*.nls.json')
+        .pipe(nls.prepareXlfFiles(transifexProjectName, transifexExtensionName))
+        .pipe(nls.pushXlfFiles(transifexApiHostname, transifexApiName, transifexApiToken));
+});
+
+gulp.task('transifex-pull', function () {
+    return nls.pullXlfFiles(transifexApiHostname, transifexApiName, transifexApiToken, vscodeLanguages, [{ name: transifexExtensionName, project: transifexProjectName }])
+        .pipe(gulp.dest(`../${transifexExtensionName}-localization`));
+});
+
+gulp.task('i18n-import', function () {
+    return gulp.src(`../${transifexExtensionName}-localization/**/*.xlf`)
+        .pipe(nls.prepareJsonFiles())
+        .pipe(gulp.dest('./i18n'));
 });
