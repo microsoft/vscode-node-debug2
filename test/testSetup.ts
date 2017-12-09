bbba915489ad26a2671eaea0b8d3c8b4c9594253
Rob Lourens <roblourens@gmail.com>
2017-12-09 13:12:55 -0800

    Find free port on each test run, should make tests more reliable

--- test/testSetup.ts
+++ test/testSetup.ts
@@ -6,17 +6,28 @@
 import * as os from 'os';
 import * as path from 'path';
 import * as ts from 'vscode-chrome-debug-core-testsupport';
+import * as findFreePort from 'find-free-port';
 
 const NIGHTLY_NAME = os.platform() === 'win32' ? 'node-nightly.cmd' : 'node-nightly';
 
-function patchLaunchArgs(launchArgs: any): void {
+function findPort(): Promise<number> {
+    return new Promise(resolve => {
+        findFreePort(9000, (err, port) => {
+            if (err) return resolve(9229);
+
+            resolve(port);
+        });
+    });
+}
+
+async function patchLaunchArgs(launchArgs: any): Promise<void> {
     launchArgs.trace = 'verbose';
     if (process.version.startsWith('v6.2')) {
         launchArgs.runtimeExecutable = NIGHTLY_NAME;
     }
 
     if (!launchArgs.port) {
-        launchArgs.port = 9229;
+        launchArgs.port = await findPort();
         launchArgs.runtimeArgs = launchArgs.runtimeArgs || [];
         launchArgs.runtimeArgs.push(`--inspect=${launchArgs.port}`, '--debug-brk');
     }
