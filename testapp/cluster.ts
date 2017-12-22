6a3075703e662719a82bcfa4bf7e95d4c4a4e5a8
Rob Lourens <roblourens@gmail.com>
2017-12-22 10:55:20 -0600

    Add cluster debug case to testapp

---
+++ testapp/cluster.ts
@@ -0,0 +1,27 @@
+import * as cluster from 'cluster';
+import * as http from 'http';
+import * as os from 'os';
+
+const numCPUs = os.cpus().length;
+
+if (cluster.isMaster) {
+    console.log(`Master ${process.pid} is running`);
+
+    // Fork workers.
+    for (let i = 0; i < numCPUs; i++) {
+        cluster.fork();
+    }
+
+    cluster.on('exit', (worker, code, signal) => {
+        console.log(`worker ${worker.process.pid} died`);
+    });
+} else {
+    // Workers can share any TCP connection
+    // In this case it is an HTTP server
+    http.createServer((req, res) => {
+        res.writeHead(200);
+        res.end('hello world\n');
+    }).listen(8000);
+
+    console.log(`Worker ${process.pid} started`);
+}
