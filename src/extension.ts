94fb5ea5c3ace46d45fd23a5d8700a9f83190c8e
Rob Lourens <roblourens@gmail.com>
2019-12-13 11:04:40 -0800

    Add debug.extensionHost.useV3 setting microsoft/vscode-js-debug#173

--- src/extension.ts
+++ src/extension.ts
@@ -7,6 +7,7 @@ import * as Core from 'vscode-chrome-debug-core';
 
 export function activate(context: vscode.ExtensionContext) {
     context.subscriptions.push(vscode.commands.registerCommand('extension.node-debug2.toggleSkippingFile', toggleSkippingFile));
+    context.subscriptions.push(vscode.debug.registerDebugConfigurationProvider('extensionHost', new ExtensionHostDebugConfigurationProvider()));
 }
 
 export function deactivate() {
@@ -21,5 +22,16 @@ function toggleSkippingFile(path: string|number): void {
     if (path && vscode.debug.activeDebugSession) {
         const args: Core.IToggleSkipFileStatusArgs = typeof path === 'string' ? { path } : { sourceReference: path };
         vscode.debug.activeDebugSession.customRequest('toggleSkipFileStatus', args);
+    }
+}
+
+class ExtensionHostDebugConfigurationProvider implements vscode.DebugConfigurationProvider {
+    resolveDebugConfiguration(_folder: vscode.WorkspaceFolder | undefined, debugConfiguration: vscode.DebugConfiguration): vscode.ProviderResult<vscode.DebugConfiguration> {
+        const useV3 = vscode.workspace.getConfiguration().get('debug.extensionHost.useV3', false);
+        if (useV3) {
+            debugConfiguration.type = 'pwa-extensionHost';
+        }
+
+        return debugConfiguration;
     }
 }
