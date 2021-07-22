021f9198e53989cc8ca5fd2024064a434414f127
Connor Peet <connor@peet.io>
2021-07-22 10:08:02 -0700

    add v2 deprecation warning

--- src/extension.ts
+++ src/extension.ts
@@ -25,14 +25,18 @@ function toggleSkippingFile(path: string | number): void {
     }
 }
 
+const useNewSettings = ['debug.extensionHost.useV3', 'debug.javascript.usePreview'];
+
 class ExtensionHostDebugConfigurationProvider implements vscode.DebugConfigurationProvider {
     resolveDebugConfiguration(folder: vscode.WorkspaceFolder | undefined, debugConfiguration: vscode.DebugConfiguration): vscode.ProviderResult<vscode.DebugConfiguration> {
-        const useV3 = getWithoutDefault('debug.extensionHost.useV3') ?? getWithoutDefault('debug.javascript.usePreview') ?? true;
+        const useV3 = useNewSettings.some(getWithoutDefault) ?? true;
 
         if (useV3) {
             folder = folder || (vscode.workspace.workspaceFolders ? vscode.workspace.workspaceFolders[0] : undefined);
             debugConfiguration['__workspaceFolder'] = folder?.uri.fsPath;
             debugConfiguration.type = 'pwa-extensionHost';
+        } else {
+            annoyingDeprecationNotification();
         }
 
         return debugConfiguration;
@@ -42,4 +46,31 @@ class ExtensionHostDebugConfigurationProvider implements vscode.DebugConfigurati
 function getWithoutDefault<T>(setting: string): T | undefined {
     const info = vscode.workspace.getConfiguration().inspect<T>(setting);
     return info?.workspaceValue ?? info?.globalValue;
+}
+
+let hasShownDeprecation = false;
+
+async function annoyingDeprecationNotification() {
+    if (hasShownDeprecation) {
+        return;
+    }
+
+    const useNewDebugger = 'Upgrade';
+    hasShownDeprecation = true;
+    const result = await vscode.window.showWarningMessage("You're using an old Node.js debugger which will be deprecated soon. Please upgrade to our new debugger, and file issues if you run into any problems", useNewDebugger);
+
+    if (result !== useNewDebugger) {
+        return;
+    }
+
+    const config = vscode.workspace.getConfiguration();
+    for (const setting of useNewSettings) {
+        const inspect = config.inspect(setting);
+        if (inspect.globalValue === false) {
+            config.update(setting, true, vscode.ConfigurationTarget.Global);
+        }
+        if (inspect.workspaceValue === false) {
+            config.update(setting, true, vscode.ConfigurationTarget.Workspace);
+        }
+    }
 }
